09d8d66d4e5dd08b76e738f1d1d5814c5c4ecc63
Oleksii Shcherbak <oleksii_shcherbak@icloud.com>
2026-01-28 11:28:25 +0200

    Add Home page with hero banner and call-to-action

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,28 @@
+import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
+import styles from './Home.module.scss';
 
 function Home() {
+    const navigate = useNavigate();
+
+    const handleNavigate = () => {
+        navigate('/catalog');
+    };
+
     return (
         <>
             <Header />
-            <div className="container">
-                <h1>Home Page</h1>
-                <p>Camper rental service</p>
+            <div className={styles.wrapper}>
+                <div className="container h-full flex flex-col justify-center">
+                    <h1 className="text-h1 text-white mb-4 max-w-[700px]">
+                        Campers of your dreams
+                    </h1>
+                    <p className="text-h2 text-white mb-10 max-w-[700px]">
+                        You can find everything you want in our catalog
+                    </p>
+                    <Button onClick={handleNavigate}>View Now</Button>
+                </div>
             </div>
         </>
     );
