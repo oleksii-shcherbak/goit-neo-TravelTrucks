664cc06e5889320beb311ab80b9a8b68ceb67e48
Oleksii Shcherbak <oleksii_shcherbak@icloud.com>
2026-01-29 00:57:15 +0200

    Fix Tailwind configuration for proper styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,25 +7,21 @@ export default {
                 main: '#101828',
                 text: '#475467',
                 gray: '#6C717B',
-                'gray-light': '#F7F7F7',
-                inputs: '#F7F7F7',
                 button: '#E44848',
                 'button-hover': '#D84343',
-                badges: '#F2F4F7',
                 rating: '#FFC531',
+                'gray-light': '#DADDE1',
+                badges: '#F2F4F7',
+                inputs: '#F7F7F7',
             },
-            fontSize: {
-                h1: ['48px', { lineHeight: '1', fontWeight: 600 }],
-                h2: ['24px', { lineHeight: '1.25', fontWeight: 600 }],
-                h3: ['20px', { lineHeight: '1.2', fontWeight: 600 }],
-                text: ['16px', { lineHeight: '1.5', fontWeight: 400 }],
+            borderColor: {
+                DEFAULT: '#DADDE1',
             },
-            container: {
-                center: true,
-                padding: '64px',
-                screens: {
-                    xl: '1440px',
-                },
+        },
+        container: {
+            center: true,
+            screens: {
+                '2xl': '1312px',
             },
         },
     },
